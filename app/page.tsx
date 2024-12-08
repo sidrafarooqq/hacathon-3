6c3020eae862ec059172086624d02e57a7301ebe
sidra farooq <fsidra167@gmail.com>
2024-12-09 01:48:51 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+
+
+import Funirofurniture from '@/components/Funirofurniture'
+import Homepage from '@/components/Home'
+import Inspiration from '@/components/inspiration'
+import Products from '@/components/product'
+
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      
+
+      <Homepage/>
+      <Products/>
+      <Inspiration/>
+      <Funirofurniture/>
+    
+      
+      
+
+
+    </div>
+  )
+}
+
+export default page
